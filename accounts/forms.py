e696ec0db6c11be2ebf48207c137b947c2921ea1
Mohsen Hassani <m.hassani.de@gmail.com>
2021-10-21 23:40:23 +0330

    Make username input lowercase in signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ from accounts import strings
 CSS_CLASS = 'form-control'
 
 class UserPassAuthForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': strings.USERNAME_FORM_LABEL, 'class': CSS_CLASS}), label=strings.USERNAME_FORM_LABEL)
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': strings.USERNAME_FORM_LABEL, 'class': CSS_CLASS, 'style': 'text-transform:lowercase;'}), label=strings.USERNAME_FORM_LABEL)
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': strings.EMAIL_FORM_LABEL, 'class': CSS_CLASS}), label=strings.EMAIL_FORM_LABEL)
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': strings.PASSWORD_FORM_LABEL, 'class': CSS_CLASS}), label=strings.PASSWORD_FORM_LABEL)
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': strings.PASSWORD_CONFIRM_FORM_LABEL, 'class': CSS_CLASS}), label=strings.PASSWORD_CONFIRM_FORM_LABEL)
