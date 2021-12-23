77fadb9259161eec072bd261b59158e487157696
Mohsen Hassani <m.hassani.de@gmail.com>
2021-12-23 11:15:33 +0330

    Add dmo settings to django admin

--- dmo/admin.py
+++ dmo/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from dmo.models import Dmo, DmoDay, Microaction
+from dmo.models import Dmo, DmoDay, Microaction, Setting
 
 # Register your models here.
 admin.site.register(Dmo)
 admin.site.register(DmoDay)
+admin.site.register(Setting)
 admin.site.register(Microaction)
