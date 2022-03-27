fcae26eed33de78e47bf264df9dbb4540a8808ea
Mohsen Hassani <m.hassani.de@gmail.com>
2022-03-27 19:01:44 +0430

    Fix issue in signals month

--- cfd/profile/views.py
+++ cfd/profile/views.py
@@ -114,9 +114,9 @@ def signals_month(request, team_id):
     else:
         month = datetime.now().replace(day=1)
     
-    analysis_type = request.GET.get('analysis_type', 'live')
+    signal_type = request.GET.get('signal_type', 'live')
     team = get_object_or_404(Team, id=team_id)
-    running_signals = Signal.signals.get_team_running_signals(team_id, analysis_type)
+    running_signals = Signal.signals.get_team_running_signals(team_id, signal_type)
     month_signals = Signal.signals.get_month_team_signals(team_id, month)
 
     data = {
