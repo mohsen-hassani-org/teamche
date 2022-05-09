3cbc30fa7abff07e4f70528df82eda0c69e52201
Mohsen Hassani <m.hassani.de@gmail.com>
2022-05-09 18:52:36 +0400

    Add vp to admin panel

--- cfd/admin.py
+++ cfd/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from cfd.models import Signal, PTAAnalysis, ClassicAnalysis, Comment
-# Register your models here.
+from cfd.models import (
+    Signal, PTAAnalysis, ClassicAnalysis, Comment, VolumeProfileAnalysis
+)
+
+
 admin.site.register(Signal)
 admin.site.register(PTAAnalysis)
 admin.site.register(ClassicAnalysis)
 admin.site.register(Comment)
+
+
+@admin.register(VolumeProfileAnalysis)
+class VolumeProfileAnalysisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'datetime', 'desc', 'user')
+    list_filter = ('user', 'datetime', 'team')
+    search_fields = ('id', 'title', 'desc')
